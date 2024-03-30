82847310f1e69f9dbcadc6a0edeff8dd6c812794
MD NURLLAH BHUIYAN <121657120+nurullah91@users.noreply.github.com>
2024-03-31 03:30:37 +0600

    Home page update for check

--- app/page.js
+++ app/page.js
@@ -1,5 +1,8 @@
 import { redirect } from 'next/navigation'
 
 export default function Home() {
-  return redirect('/duas/category-1');
+  // return redirect('/duas/category-1');
+  return(
+    <div><h2 className="text-4xl text-center text-green-400">Welcome to Dua Home page</h2></div>
+  )
 }
